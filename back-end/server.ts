ce0e8c2aa9cd2d1d83df69f54fbe7e72278b959b
Alan <alanleingpro@gmail.com>
2024-09-23 15:12:40 +0200

    Refactor server.ts and roomSelection.tsx files

--- back-end/server.ts
+++ back-end/server.ts
@@ -1,38 +1,33 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import http from 'http';
-import { Server } from 'socket.io'; 
-import socketConfig from './socketConfig'; 
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import http from "http";
+import { Server } from "socket.io";
+import socketConfig from "./socketConfig";
 
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST'],
-}));
+app.use(cors());
 
 const server = http.createServer(app);
 
 app.use(express.json());
 
-const MONGO_URI = process.env.MONGO_URI || '';
+const MONGO_URI = process.env.MONGO_URI || "";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(MONGO_URI)
-
+mongoose.connect(MONGO_URI);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
   },
 });
 
 socketConfig(io);
-
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
